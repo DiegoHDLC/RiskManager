0d9ccf996b971d3e9a7e793c931677cca76b5842
DiegoHDLC <dherreradelacalle@gmail.com>
2022-07-13 20:47:40 -0400

    first commit

---
+++ Plan.cs
@@ -0,0 +1,15 @@
+namespace GestionRiesgos
+{
+    class Plan
+    {
+        public int id;
+        public string descripcion;
+        public Plan(int id, string descripcion)
+        {
+            this.id = id;
+            this.descripcion = descripcion;
+        }
+    }
+
+    
+}
